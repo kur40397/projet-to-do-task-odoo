51513618b6e4e0f1a7fccf3664ea787a6509bb67
badr <badr.ahrir1@gmail.com>
2024-12-18 10:30:52 +0000

    second commit

--- models/task.py
+++ models/task.py
@@ -7,15 +7,16 @@ from odoo import models, fields, api
 class Task(models.Model):
     _name = 'todo_app.task'
     _description = 'this a todo app'
-    name= fields.Char(required=True,string='Name')
-    assignTo=fields.Many2one('res.partner',string='Assign To')
-    description=fields.Char(string='Description')
-    dueDate=fields.Date(string='Due Date')
+    _inherit = ['mail.thread','mail.activity.mixin']
+    name= fields.Char(required=True,string='Name', tracking=True)
+    assignTo=fields.Many2one('res.partner',string='Assign To' ,tracking=True)
+    description=fields.Char(string='Description' ,tracking=True)
+    dueDate=fields.Date(string='Due Date', tracking=True)
     status=fields.Selection(
         [
             ('new','New'),
             ('inProgress','In Progress'),
             ('completed','Completed'),
-        ],default='new'
+        ],default='new', tracking=True
     )
 
