ff81b56301301d43780025785bd79444e57d820e
badr <badr.ahrir1@gmail.com>
2024-12-18 16:06:48 +0000

    commit last one

--- models/task.py
+++ models/task.py
@@ -8,6 +8,7 @@ class Task(models.Model):
     _name = 'todo_app.task'
     _description = 'this a todo app'
     _inherit = ['mail.thread','mail.activity.mixin']
+    _rec_name = 'description'
     name= fields.Char(required=True,string='Name', tracking=True)
     assignTo=fields.Many2one('res.partner',string='Assign To' ,tracking=True)
     description=fields.Char(string='Description' ,tracking=True)
@@ -17,13 +18,21 @@ class Task(models.Model):
             ('new','New'),
             ('inProgress','In Progress'),
             ('completed','Completed'),
+            ('closed','Closed')
         ],default='new', tracking=True ,readonly=True, copy=False
 
     )
+    active=fields.Boolean(default=True)
 
     def button_in_progress(self):
         self.write({'status': "inProgress"})
 
     def button_completed(self):
         self.write({'status':'completed'})
+
+    def action_closed(self):
+        for rec in self:
+            self.write({
+                'status':'closed'
+            })
 
