1601d51f13fb37e75f76910c1a2c9bfc4b270226
badr <badr.ahrir1@gmail.com>
2024-12-20 20:39:25 +0000

    reports

--- models/task.py
+++ models/task.py
@@ -6,9 +6,9 @@ from odoo import models, fields, api
 
 class Task(models.Model):
     _name = 'todo_app.task'
-    _description = 'this a todo app'
+    _description = 'this a todo app' # humain readable model name
     _inherit = ['mail.thread','mail.activity.mixin']
-    _rec_name = 'description'
+    _rec_name = 'description' # le field li radi afficha comme un nom du record
     name= fields.Char(required=True,string='Name', tracking=True)
     assignTo=fields.Many2one('res.partner',string='Assign To' ,tracking=True)
     description=fields.Char(string='Description' ,tracking=True)
@@ -20,9 +20,10 @@ class Task(models.Model):
             ('completed','Completed'),
             ('closed','Closed')
         ],default='new', tracking=True ,readonly=True, copy=False
-
     )
     active=fields.Boolean(default=True)
+
+    is_late=fields.Boolean()
 
     def button_in_progress(self):
         self.write({'status': "inProgress"})
@@ -35,4 +36,16 @@ class Task(models.Model):
             self.write({
                 'status':'closed'
             })
+
+    def check_expected_date(self):
+        print(self)
+        # dans les taches automatiser self fait reference au model
+        tasks_ids=self.search([])
+        for rec in tasks_ids:
+            if rec.dueDate  and rec.dueDate < fields.date.today():
+                rec.is_late=True
+                rec.write({
+                    'is_late':True,
+                    'status':'closed'
+                })
 
